0741c72e4ca26db89a1f86d563bed7b537291f65
Reda Boutayeb <87339534+RedaB2@users.noreply.github.com>
2024-01-25 14:25:01 -0500

    fix(frontend): replaced with correct map and made smaller (#12)
    * replaced map and made smaller
    
    * map file

--- apps/frontend/src/routes/MapRoute.tsx
+++ apps/frontend/src/routes/MapRoute.tsx
@@ -1,11 +1,11 @@
-import imgUrl from "./MapImg.jpg";
+import imgUrl from "./MapImg.png";
 import CSVDataDisplay from "./CSVData.tsx";
 
 export default function MapRoute() {
   return (
     <>
       <div>
-        <img src={imgUrl} alt="Map 2nd Floor" />
+        <img src={imgUrl} alt="Map 1st Floor" width="500px" height="auto" />
       </div>
       <CSVDataDisplay></CSVDataDisplay>
     </>
