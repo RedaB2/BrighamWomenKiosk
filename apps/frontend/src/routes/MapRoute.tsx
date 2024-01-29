995d2607a7521846ba258bcfa8ab0986b078d178
MJudyCode <93202545+MJudyCode@users.noreply.github.com>
2024-01-29 17:16:51 -0500

    Menu Bar and Buttons on Map Page

--- apps/frontend/src/routes/MapRoute.tsx
+++ apps/frontend/src/routes/MapRoute.tsx
@@ -1,10 +1,25 @@
 import imgUrl from "./MapImg.png";
+import "./menubar.css";
 
 export default function MapRoute() {
   return (
     <>
+      <div className="menubar">
+        <a className="active" href="#home">
+          Home
+        </a>
+        <a href="#map">Map</a>
+        <a href="#csv">CSV</a>
+        <a href="#serviceRequest">Service Request</a>
+      </div>
       <div>
         <img src={imgUrl} alt="Map 1st Floor" width="500px" height="auto" />
+      </div>
+      <div>
+        <button>Home</button>
+        <button>Map</button>
+        <button>CSV</button>
+        <button>Service Requests</button>
       </div>
     </>
   );
