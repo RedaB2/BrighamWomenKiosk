3c4647ba955cc1573a88a186403c52c497634b72
MJudyCode <93202545+MJudyCode@users.noreply.github.com>
2024-01-30 17:50:59 -0500

    Menu Bar and Buttons on Map Page

--- apps/frontend/src/routes/MapRoute.tsx
+++ apps/frontend/src/routes/MapRoute.tsx
@@ -1,26 +1,75 @@
 import imgUrl from "./MapImg.png";
-import "./menubar.css";
+import "./sideBar.css";
+import React, { FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function MapRoute() {
+function openNav() {
+  const name = document.getElementById("mySidebar");
+  if (name != null) {
+    name.style.width = "250px";
+  }
+}
+
+function closeNav() {
+  const name = document.getElementById("mySidebar");
+  if (name != null) {
+    name.style.width = "250px";
+  }
+}
+
+const MapRoute = () => {
+  const navigate = useNavigate();
+
+  const handleSignIn = (e: FormEvent) => {
+    e.preventDefault();
+    navigate("/sign-in");
+  };
+  const handleMapPage = (e: FormEvent) => {
+    e.preventDefault();
+    navigate("/");
+  };
+  const handleCSV = (e: FormEvent) => {
+    e.preventDefault();
+    navigate("/csv-data");
+  };
+  const handleServiceRequests = (e: FormEvent) => {
+    e.preventDefault();
+    navigate("/service-request");
+  };
+
   return (
     <>
-      <div className="menubar">
-        <a className="active" href="#home">
-          Home
+      <div id="mySidebar" className="sidebar">
+        <a href="#" className="closebtn" onClick={closeNav}>
+          &times;
         </a>
-        <a href="#map">Map</a>
-        <a href="#csv">CSV</a>
-        <a href="#serviceRequest">Service Request</a>
+        <a href="/sign-in">Sign In</a>
+        <a href="/">Map</a>
+        <a href="/csv-data">CSV Data</a>
+        <a href="/service-requests">Service Requests</a>
       </div>
+
+      <button className="openbtn" onClick={openNav}>
+        &#9776; Menu
+      </button>
       <div>
         <img src={imgUrl} alt="Map 1st Floor" width="500px" height="auto" />
       </div>
       <div>
-        <button>Home</button>
-        <button>Map</button>
-        <button>CSV</button>
-        <button>Service Requests</button>
+        <button type="button" role="link" onClick={handleSignIn}>
+          Sign In
+        </button>
+        <button type="button" role="link" onClick={handleMapPage}>
+          Map
+        </button>
+        <button type="button" role="link" onClick={handleCSV}>
+          CSV
+        </button>
+        <button type="button" role="link" onClick={handleServiceRequests}>
+          Service Requests
+        </button>
       </div>
     </>
   );
-}
+};
+export default MapRoute;
